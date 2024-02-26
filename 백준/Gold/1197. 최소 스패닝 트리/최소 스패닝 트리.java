10328c31ae327f8103401507524028cd0059f5c4
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-02-26 13:32:27 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 472 ms, Memory: 48776 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,72 +1,73 @@
 import java.util.*;
 import java.io.*;
 
-// 최소 스패닝 트리 
+// 최소 스패닝 트리 (프림)
 public class Main {
-	static class Edge implements Comparable<Edge>{
-		int from, to, weight;
-
-		public Edge(int from, int to, int weight) {
+	static StringBuilder sb = new StringBuilder();
+	static int V, E;
+	static boolean[] vis;
+	static Vertex[] adjList;
+	static int[] minEdge;
+	static class Vertex implements Comparable<Vertex>{
+		int num, weight;
+		Vertex next;
+		public Vertex(int num, int weight, Vertex next) {
 			super();
-			this.from = from;
-			this.to = to;
+			this.num = num;
 			this.weight = weight;
+			this.next = next;
 		}
-
 		@Override
-		public int compareTo(Edge o) {
+		public int compareTo(Vertex o) {
 			return Integer.compare(this.weight, o.weight);
 		}
 		
 	}
-	static StringBuilder sb = new StringBuilder();
-	static int V, E;
-	static Edge[] edgeList;
-	static int[] parents;
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());	// 정점 개수 
-		E = Integer.parseInt(st.nextToken());	// 간선 개수 
-		edgeList = new Edge[E];
+		
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		vis = new boolean[V+1];	// 트리 정점에 속하는지 여부 체크 
+		adjList = new Vertex[V+1];
+		minEdge = new int[V+1];
 		
 		for (int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken())-1;
-			int to = Integer.parseInt(st.nextToken())-1;
-			int weight = Integer.parseInt(st.nextToken());	// 음수일 수도 있음 
-			edgeList[i] = new Edge(from, to, weight);
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			adjList[from] = new Vertex(to, weight, adjList[from]);
+			adjList[to] = new Vertex(from, weight, adjList[to]);
 		}
 		
-		// make-set 
-		parents = new int[V];
-		for (int i=0; i<V; i++) {
-			parents[i] = i;
-		}
 		
-		Arrays.sort(edgeList);	// 가중치 기준 오름차순 정렬 
-		
-		int cnt = 0;
+		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+		Arrays.fill(minEdge, Integer.MAX_VALUE);
+		minEdge[1] = 0;
+		pq.offer(new Vertex(1, minEdge[1], null));
 		int result = 0;
-		for (Edge edge : edgeList) {
-			if (!union(edge.from, edge.to))	continue;
-			cnt++;
-			result += edge.weight;
-			if (cnt == V - 1)	break;
+		
+		while(!pq.isEmpty()){
+			Vertex minVertex = pq.poll();
+			if (vis[minVertex.num])	continue;
+			
+			vis[minVertex.num] = true;
+			result += minEdge[minVertex.num];
+			
+			for (Vertex tmp = adjList[minVertex.num]; tmp != null; tmp = tmp.next) {
+				if (vis[tmp.num])	continue;
+				if (minEdge[tmp.num] > tmp.weight) {
+					minEdge[tmp.num] = tmp.weight;
+					pq.offer(new Vertex(tmp.num, minEdge[tmp.num], null));
+				}
+			}
 		}
+
 		
 		System.out.println(result);
 	}
-	static int findSet(int a) {
-		if (parents[a] == a)	return a;
-		return parents[a] = findSet(parents[a]);
-	}
-	static boolean union(int a, int b) {
-		int rootA = findSet(a);
-		int rootB = findSet(b);
-		if (rootA == rootB)	return false;
-		parents[rootA] = rootB;
-		return true;
-	}
+
 }
