d391bf5483914ea6842a831238970422d322ff84
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-04-02 12:34:09 +0900

    [Silver V] Title: 피보나치 수 4, Time: 256 ms, Memory: 24956 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -14,12 +14,8 @@ public class Main {
 		arr[0] = new BigInteger("0");
         if (N != 0) arr[1] = new BigInteger("1");
 		
-		for (int i=2; i<=N; i++) {
-			if (i %2 == 0) {
-				arr[i] = arr[i/2].multiply(arr[i/2]).add(arr[i/2].multiply(arr[i/2-1]).multiply(new BigInteger("2")));
-			} else {
-				arr[i] = arr[i/2].multiply(arr[i/2]).add(arr[i/2+1].multiply(arr[i/2+1]));
-			}
+		for (int i = 2; i<=N; i++) {
+			arr[i] = arr[i-1].add(arr[i-2]);
 		}
 		
 
