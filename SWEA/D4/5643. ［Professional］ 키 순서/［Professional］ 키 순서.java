049b9c0c46211dab7bc1cc8201c22e6e3e70fb04
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-04-03 14:47:00 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,515 ms, Memory: 109,100 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+// 키 순서 
+public class Solution {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int res;			// 자신의 키 순서 정확히 알 수 있는 학생 수 (결과) 
+	static boolean[] vis;
+	static Node[] std;		// 학생들의 번호와 키 관계 저장 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append("#").append(tc).append(" ");
+			N = Integer.parseInt(br.readLine());	// 학생 수 
+			std = new Node[N];
+			
+			M = Integer.parseInt(br.readLine());	// 두 학생 키 비교한 횟수 
+			for (int i = 0 ; i < N; i++) {
+				std[i] = new Node(i);		// std배열 초기화 
+			}
+			res = 0;
+			StringTokenizer st = null;
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int shorter = Integer.parseInt(st.nextToken()) - 1;
+				int taller = Integer.parseInt(st.nextToken()) - 1;
+				
+				// 두 학생 키 순서 양방향 저장 
+				std[shorter].high.add(taller);
+				std[taller].low.add(shorter);
+			}
+			
+			for (int i = 0 ; i < N ; i++) {
+				vis = new boolean[N];
+				
+				find(i);	// 각 학생마다 정확한 키 순서 알 수 있는지 탐색 
+			}
+			sb.append(res).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+	private static void find(int num) {
+		Queue<Node> queue = new ArrayDeque<>();
+		queue.offer(std[num]);
+		vis[num] = true;
+		// 해당 학생보다 작은 학생 탐색 
+		while (!queue.isEmpty()) {
+			Node cur = queue.poll();
+			for (int d = 0; d < cur.low.size(); d++) {
+				int nxt = cur.low.get(d);
+				if (vis[nxt])	continue;
+				vis[nxt] = true;
+				queue.offer(std[nxt]);
+			}
+		}
+		// 해당 학생보다 큰 학생 탐색 
+		queue.offer(std[num]);
+		while(!queue.isEmpty()) {
+			Node cur = queue.poll();
+			for (int d = 0; d < cur.high.size(); d++) {
+				int nxt = cur.high.get(d);
+				if (vis[nxt])	continue;
+				vis[nxt] = true;
+				queue.offer(std[nxt]);
+			}
+		}
+		
+		// 모든 학생 방문했는지 체크
+		boolean isPossible = true;
+		for (int i = 0 ; i<N; i++) {
+			if (!vis[i])	isPossible = false;
+		}
+		if (isPossible)	res++;	// 모든학생방문 == 모든학생과 비교하여 자신의 키 순서 알 수 있음 
+	}
+	
+	static class Node{
+		int num;
+		ArrayList<Integer> low, high;
+		public Node(int num) {
+			super();
+			this.num = num;
+			low = new ArrayList<>();
+			high = new ArrayList<>();
+		}
+	}
+}
