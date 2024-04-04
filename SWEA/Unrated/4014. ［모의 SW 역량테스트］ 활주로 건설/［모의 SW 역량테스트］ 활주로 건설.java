10f32b38f4030096e626dfd2d4fc64c59542ea79
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-04-04 15:37:50 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 129 ms, Memory: 29,476 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -1,22 +1,22 @@
 import java.util.*;
 import java.io.*;
 
-// 활주로 건설 
 public class Solution {
 	static StringBuilder sb = new StringBuilder();
 	static int N, X;
 	static int[][] map;
 	static int res;
-
+	static boolean[] vis;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+
 		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken()); // 지도 한 변의 크기
-			X = Integer.parseInt(st.nextToken()); // 경사로의 길이
+			N = Integer.parseInt(st.nextToken()); // map크기 NxN
+			X = Integer.parseInt(st.nextToken()); // 경사로 길이
 			map = new int[N][N];
+
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
@@ -25,124 +25,161 @@ public class Solution {
 			}
 
 			res = 0;
-			for (int i = 0; i < N; i++) {
-				int std = map[i][0]; // 현재 기준값
-				int cnt = 1; // 현재보다 큰 수 만났을 때 지나온 만큼을 X와 비교하기 위해
-//				int length = 1;	// 현재보다 작은 수 만났을 때 최소 길이를 X와 비교하기 위해 
-				boolean runway = false;
-				boolean isPossible = true;
+			func();
 
-				for (int j = 1; j < N; j++) {
-					if (map[i][j] == std) {
-						cnt++;
-						if (runway && cnt == X) {
-							runway = false;
-							cnt = 0;
-						}
-						if ((j == N - 1) && runway) {
-							if (cnt < X) {
-								isPossible = false;
-							}
-						}
-
-					} else if (map[i][j] == std - 1) { // 기준보다 작은애 만나면
-						if (runway) {
-							if (cnt < X) { // 경사로 길이에 미치지 못하고 보다 작은 높이 나오면 경사로 설치 불가능
-								isPossible = false;
-								break;
-							} else { // 경사로 하나 설치가능한 상태
-								std = map[i][j];
-								cnt = 1;
-							}
-						} else {
-							cnt = 1;
-							std = map[i][j];
-							runway = true;
-							if (j == N - 1) {
-								if (cnt < X) {
-									isPossible = false;
-								}
-							}
-						}
-					} else if (map[i][j] == std + 1) { // 기준보다 큰 애 만나면
-						if (cnt >= X) {
-							std = map[i][j];
-							cnt = 1;
-						} else {
-							isPossible = false;
-							break;
-						}
-					} else {
-						isPossible = false;
-						break;
-					}
-				}
-
-				if (isPossible) {
-					res++;
-				}
-			}
-			
-			for (int c = 0; c < N; c++) {
-				int std = map[0][c]; // 현재 기준값
-				int cnt = 1; // 현재보다 큰 수 만났을 때 지나온 만큼을 X와 비교하기 위해
-				boolean runway = false;
-				boolean isPossible = true;
-
-				for (int r = 1; r < N; r++) {
-					if (map[r][c] == std) {
-						cnt++;
-						if (runway && cnt == X) {
-							runway = false;
-							cnt = 0;
-						}
-						if ((r == N - 1) && runway) {
-							if (cnt < X) {
-								isPossible = false;
-							}
-						}
-
-					} else if (map[r][c] == std - 1) { // 기준보다 작은애 만나면
-						if (runway) {
-							if (cnt < X) { // 경사로 길이에 미치지 못하고 보다 작은 높이 나오면 경사로 설치 불가능
-								isPossible = false;
-								break;
-							} else { // 경사로 하나 설치가능한 상태
-								std = map[r][c];
-								cnt = 1;
-							}
-						} else {
-							cnt = 1;
-							std = map[r][c];
-							runway = true;
-							if (r == N - 1) {
-								if (cnt < X) {
-									isPossible = false;
-								}
-							}
-						}
-					} else if (map[r][c] == std + 1) { // 기준보다 큰 애 만나면
-						if (cnt >= X) {
-							std = map[r][c];
-							cnt = 1;
-						} else {
-							isPossible = false;
-							break;
-						}
-					} else {
-						isPossible = false;
-						break;
-					}
-				}
-
-				if (isPossible) {
-					res++;
-				}
-			}
-
-			sb.append(res).append("\n");
+			sb.append("#").append(tc).append(" ").append(res).append("\n");
 		}
 		System.out.println(sb);
+		br.close();
 	}
 
+	private static void func() {
+		boolean isPossible = true;
+		// 가로 확인
+		for (int i = 0; i < N; i++) {
+			vis = new boolean[N];
+			// 앞->뒤
+			isPossible = true;
+			boolean flag = false;
+			int std = map[i][0];
+			int cnt = 0; // 경사로 길이카운트
+			for (int j = 1; j < N; j++) {
+				if (flag) { // 경사로 설치중
+					if (map[i][j] != std - 1) { // 설치 불가
+						isPossible = false;
+						break;
+					}
+					cnt++;
+					if (cnt == X) { // 경사로 길이 끝
+						for (int k=j; k>j-cnt; k--) {
+							vis[k] = true;
+						}
+						flag = false;
+						std = map[i][j];
+						cnt = 0;
+					}
+				} else { // 경사로 설치전
+					if (map[i][j] == std - 1) { // 기준보다 작은값나오면 경사로(내리막) 설치
+						cnt++;
+						flag = true;
+					} else if (map[i][j] > std) {
+						std = map[i][j];
+					} else if (map[i][j] < std - 1) {
+						isPossible = false;
+						break;
+					}
+				}
+			}
+			if (flag && cnt < X)
+				isPossible = false;
+			if (!isPossible)
+				continue;
+			// 뒤->앞
+			flag = false;
+			std = map[i][N - 1];
+			cnt = 0; // 경사로 길이카운트
+			for (int j = N - 2; j >= 0; j--) {
+				if (flag) { // 경사로 설치중
+					if (map[i][j] != std - 1 || vis[j]) {
+						isPossible = false;
+						break;
+					}
+					cnt++;
+					if (cnt == X) { // 이전까지 경사로 길이 끝
+						flag = false;
+						std = map[i][j];
+						cnt = 0;
+					}
+				} else { // 경사로 설치전
+					if (map[i][j] == std - 1 && !vis[j]) { // 기준보다 작은값나오면 경사로(내리막) 설치
+						cnt++;
+						flag = true;
+					} else if (map[i][j] > std) {
+						std = map[i][j];
+					} else if (map[i][j] < std - 1 || (map[i][j] == std-1 && vis[j])) {
+						isPossible = false;
+						break;
+					}
+				}
+			}
+			if (flag && cnt < X)
+				isPossible = false;
+			if (isPossible)
+				res++;
+		}
+
+		// 세로 확인
+		for (int c = 0; c < N; c++) {
+			vis = new boolean[N];
+			// 위->아래
+			isPossible = true;
+			boolean flag = false;
+			int std = map[0][c];
+			int cnt = 0; // 경사로 길이카운트
+			for (int r = 1; r < N; r++) {
+				if (flag) { // 경사로 설치중
+					if (map[r][c] != std - 1) {
+						isPossible = false;
+						break;
+					}
+					cnt++;
+					if (cnt == X) { // 이전까지 경사로 길이 끝
+						for (int k=r; k>r-cnt; k--) {
+							vis[k] = true;
+						}
+						flag = false;
+						std = map[r][c];
+						cnt = 0;
+
+					} 
+				} else { // 경사로 설치전
+					if (map[r][c] == std - 1) { // 기준보다 작은값나오면 경사로(내리막) 설치
+						cnt++;
+						flag = true;
+					} else if (map[r][c] > std) {
+						std = map[r][c];
+					} else if (map[r][c] < std - 1) {
+						isPossible = false;
+						break;
+					}
+				}
+			}
+			if (flag && cnt < X)
+				isPossible = false;
+			if (!isPossible)
+				continue;
+			// 아래->위
+			flag = false;
+			std = map[N - 1][c];
+			cnt = 0; // 경사로 길이카운트
+			for (int r = N - 2; r >= 0; r--) {
+				if (flag) { // 경사로 설치중
+					if (map[r][c] != std - 1 || vis[r]) {
+						isPossible = false;
+						break;
+					}
+					cnt++;
+					if (cnt == X) { // 이전까지 경사로 길이 끝
+						flag = false;
+						std = map[r][c];
+						cnt = 0;
+					} 
+				} else { // 경사로 설치전
+					if (map[r][c] == std - 1 && !vis[r]) { // 기준보다 작은값나오면 경사로(내리막) 설치
+						cnt++;
+						flag = true;
+					} else if (map[r][c] > std) {
+						std = map[r][c];
+					} else if (map[r][c] < std - 1 || (map[r][c] == std-1 && vis[r])) {
+						isPossible = false;
+						break;
+					}
+				}
+			}
+			if (flag && cnt < X)
+				isPossible = false;
+			if (isPossible)
+				res++;
+		}
+	}
 }
