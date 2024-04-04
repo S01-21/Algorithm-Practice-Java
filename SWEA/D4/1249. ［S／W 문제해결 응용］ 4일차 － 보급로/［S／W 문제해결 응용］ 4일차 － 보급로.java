99cff22a4c8329686f94e7753cbbc023065e797c
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-04-04 22:40:07 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 668 ms, Memory: 26,872 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -10,6 +10,19 @@ public class Solution {
 	static int[][] minDist;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
+	static class Node implements Comparable<Node>{
+		int x, y, dist;
+		public Node(int x, int y, int dist) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.dist, o.dist);
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
@@ -20,28 +33,46 @@ public class Solution {
 			map = new int[N][N];
 			minDist = new int[N][N];
 			vis = new boolean[N][N];
-			
-			
+
 			for (int i=0; i<N; i++) {
 				String str = br.readLine();
 				for (int j=0 ;j<N; j++) {
 					map[i][j] = str.charAt(j)-'0';
+					minDist[i][j] = Integer.MAX_VALUE;
 				}
 			}
 			
-			
-			dijkstra();
-			
+			//dijkstra();
+			dijkstraPQ();
 			
 			sb.append("#").append(tc).append(" ").append(res).append("\n");
 		}
 		
 		System.out.println(sb);
 	}
-	private static void dijkstra() {
-		for (int i=0; i<N; i++) {
-			Arrays.fill(minDist[i], Integer.MAX_VALUE);
+	private static void dijkstraPQ() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		minDist[0][0] = map[0][0];
+		pq.offer(new Node(0,0,minDist[0][0]));
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if (minDist[cur.x][cur.y] != cur.dist)	continue;
+			
+			for (int dir = 0; dir < 4; dir++) {
+				int nx = cur.x + dx[dir];
+				int ny = cur.y + dy[dir];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N)	continue;
+				if (minDist[nx][ny] > minDist[cur.x][cur.y] + map[nx][ny] ) {
+					minDist[nx][ny] = minDist[cur.x][cur.y] + map[nx][ny];
+					pq.offer(new Node(nx, ny, minDist[nx][ny]));
+				}
+			}
 		}
+		res = minDist[N-1][N-1];
+	}
+	private static void dijkstra() {
 		minDist[0][0] = 0;
 		vis[0][0] = true;
 		
@@ -85,4 +116,5 @@ public class Solution {
 		}
 		res = minDist[N-1][N-1];
 	}
+	
 }
