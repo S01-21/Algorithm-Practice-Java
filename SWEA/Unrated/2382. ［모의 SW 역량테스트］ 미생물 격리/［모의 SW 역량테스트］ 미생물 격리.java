93ee9a08604a1489fe5c17b382cb419ec858dd24
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-04-08 22:04:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 721 ms, Memory: 103,632 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,160 +1,124 @@
+
 import java.util.*;
 import java.io.*;
 
-// 미생물 격리
 public class Solution {
 	static StringBuilder sb = new StringBuilder();
 	static int N, M, K;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 }; // 상(0)-하(1)-좌(2)-우(3)
-	static Bug[] bugs;
-	static int res; // M시간 후 남아 있는 미생물 수 총합 (결과)
+	static int res;
+	static int[] dx = { -1, 1, 0, 0 }; // 상-하-좌-우
+	static int[] dy = { 0, 0, -1, 1 };
+	static PriorityQueue<Microbe> pq, pq2;
+	static Microbe[][] map;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+	static class Microbe implements Comparable<Microbe> {
+		int x, y, c, d;
 
-		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken()); // 셀의 개수
-			M = Integer.parseInt(st.nextToken()); // 격리 시간
-			K = Integer.parseInt(st.nextToken()); // 미생물 군집의 개수
-
-			bugs = new Bug[K];
-
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int amount = Integer.parseInt(st.nextToken());
-				int dir = Integer.parseInt(st.nextToken()) - 1;
-				bugs[i] = new Bug(x, y, amount, dir, true);
-			}
-			for (int i = 0; i < M; i++) {
-				moveBug();
-				checkConflict();
-			}
-
-			res = 0;
-			countBug();
-			sb.append(res).append("\n");
-		}
-		System.out.println(sb);
-	}
-
-	private static void checkConflict() {
-
-		for (int i = 0; i < K; i++) {
-			if (!bugs[i].alive)
-				continue;
-			int max = i;
-			Bug bugA = bugs[i];
-			int sum = bugA.amount;
-
-			for (int j = i + 1; j < K; j++) {
-				Bug bugB = bugs[j];
-				if (!bugB.alive) continue;
-				if (bugA.x == bugB.x && bugA.y == bugB.y) { // 충돌지점
-					sum += bugB.amount;
-					if (bugs[max].amount < bugB.amount) {
-						max = j;
-					}
-				}
-			}
-
-			for (int j = i; j < K; j++) {
-				Bug bugB = bugs[j];
-				if (bugA.x == bugB.x && bugA.y == bugB.y) {
-					if (!bugB.alive) continue;
-					if (j == max) {
-						bugB.amount = sum;
-					}
-					if (j != max) {
-						bugB.alive = false;
-					}
-				}
-
-			}
-		}
-
-	}
-
-	private static void countBug() {
-		for (Bug bug : bugs) {
-			if (!bug.alive)
-				continue;
-			res += bug.amount;
-		}
-	}
-
-	private static void moveBug() {
-		for (Bug bug : bugs) {
-			if (!bug.alive)
-				continue; // 없어진 미생물이면 패스
-			int nx = bug.x + dx[bug.dir];
-			int ny = bug.y + dy[bug.dir];
-			if (isOut(nx, ny))
-				continue;
-			bug.x = nx;
-			bug.y = ny;
-			if (isBoundary(nx, ny)) { // 약품에 닿았을 경우
-				bug.amount /= 2; // 미생물 수 절반으로 줄이기
-				// 반대방향으로
-				switch (bug.dir) {
-				case 0:
-				case 2:
-					bug.dir++;
-					break;
-				case 1:
-				case 3:
-					bug.dir--;
-					break;
-				}
-				if (bug.amount == 0) {
-					bug.alive = false;
-					continue;
-				}
-				continue;
-			}
-		}
-	}
-
-	static boolean isOut(int x, int y) {
-		return x < 0 || x >= N || y < 0 || y >= N;
-	}
-
-	static boolean isBoundary(int x, int y) {
-		return x == 0 || x == N - 1 || y == 0 || y == N - 1;
-	}
-
-	static class Pair {
-		int x, y;
-
-		public Pair(int x, int y) {
+		public Microbe(int x, int y, int c, int d) {
 			super();
 			this.x = x;
 			this.y = y;
+			this.c = c; // 미생물 수
+			this.d = d; // 이동 방향
 		}
 
-	}
-
-	static class Bug {
-		int x, y, amount, dir;
-		boolean alive;
-
-		public Bug(int x, int y, int amount, int dir, boolean alive) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.amount = amount;
-			this.dir = dir;
-			this.alive = alive;
+		@Override
+		public int compareTo(Microbe o) {
+			return Integer.compare(o.c, this.c);
 		}
 
 		@Override
 		public String toString() {
-			return "Bug [(" + x + ", " + y + "), amount=" + amount + ", dir=" + dir + ", " + alive + "] ";
+			return "Microbe [x=" + x + ", y=" + y + ", c=" + c + ", d=" + d + "]";
 		}
+		
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // NxN map
+			M = Integer.parseInt(st.nextToken()); // 격리 시간
+			K = Integer.parseInt(st.nextToken()); // 최초 미생물 군집 개수
+			
+
+			pq = new PriorityQueue<>();
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				int d = Integer.parseInt(st.nextToken()) - 1;
+				pq.offer(new Microbe(x, y, c, d));
+			}
+
+			for (int i = 0; i < M; i++) {
+				func();
+			}
+
+			res = 0;
+			while(!pq.isEmpty()) {
+				res += pq.poll().c;
+			}
+
+			sb.append('#').append(tc).append(" ").append(res).append("\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
+	private static void func() {
+		pq2 = new PriorityQueue<>();
+		map = new Microbe[N][N];
+		while (!pq.isEmpty()) {
+			// 이동방향으로 이동
+			Microbe cur = pq.poll();
+			int nx = cur.x + dx[cur.d];
+			int ny = cur.y + dy[cur.d];
+
+			// 약품에 닿았을 경우
+			if (nx == 0 || nx == N - 1 || ny == 0 || ny == N - 1) {
+				cur.c /= 2;
+				if (cur.d == 0 || cur.d == 2) {
+					cur.d++;
+				} else if (cur.d == 1 || cur.d == 3) {
+					cur.d--;
+				} // 이동방향 반대로
+				
+				if (cur.c == 0) { // 미생물수 0 되면 군집 사라짐
+					continue;
+				} else {
+					if (map[nx][ny] != null) {	// 이미 다른 군집이 이동했을 경우
+						map[nx][ny].c += cur.c;
+						continue;
+					} else {
+						map[nx][ny] = new Microbe(nx, ny, cur.c, cur.d);
+						continue;
+					}
+				}
+			} else {	// 약품 아닌 곳
+				// 이미 다른 군집이 이동했을 경우
+				if (map[nx][ny] != null) {
+					map[nx][ny].c += cur.c;
+					continue;
+				} else {
+					// 빈칸에 무사 이동할 경우
+					map[nx][ny] = new Microbe(nx, ny, cur.c, cur.d);
+				}
+			}
+		}
+		
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				if (map[i][j] != null) {
+					pq2.offer(map[i][j]);
+				}
+			}
+		}
+		
+		pq = pq2;
 	}
 }
