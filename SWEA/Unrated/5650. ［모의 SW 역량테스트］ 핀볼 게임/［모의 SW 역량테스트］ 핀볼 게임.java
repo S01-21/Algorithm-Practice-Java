395ac2743c9f4488e834e5d19543260ed9b59f5f
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-04-09 21:40:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 2,258 ms, Memory: 51,340 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -1,146 +1,92 @@
 import java.util.*;
 import java.io.*;
 
-// 핀볼 게임
+// 핀볼 게임 
 public class Solution {
 	static StringBuilder sb = new StringBuilder();
 	static int N;
-	static int[][] board;
-	static int[] dx = { -1, 0, 1, 0 }; // 상(0)-우(1)-하(2)-좌(3)
-	static int[] dy = { 0, 1, 0, -1 };
-
-	static class Pair {
-		int x, y;
-		public Pair(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	static ArrayList<Pair>[] wormhole;
-	static int maxScore;
-
+	static int[][] map;
+	static int res;
+	static int[][] worm;
+	static int[] dx = {-1,1,0,0};	// 상하좌우 (0,1,2,3)
+	static int[] dy = {0,0,-1,1};
+	static int[][] dd = {{0,1,2,3}, {1, 3, 0, 2}, {3, 0, 1, 2}, {2, 0, 3, 1}, {1, 2, 3, 0}, {1,0,3,2}};	// 1~5번 블록 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine().trim());
+		StringTokenizer st = null;
+		
 		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
 			N = Integer.parseInt(br.readLine().trim());
-			board = new int[N][N];
-			wormhole = new ArrayList[11];
-			for (int i = 0; i <= 10; i++) {
-				wormhole[i] = new ArrayList<>();
-			}
-			StringTokenizer st = null;
-			for (int i = 0; i < N; i++) {
+			map = new int[N+2][N+2];
+			worm = new int[11][4];
+			for (int i=1; i<= N; i++) {
 				st = new StringTokenizer(br.readLine().trim());
-				for (int j = 0; j < N; j++) {
-					board[i][j] = Integer.parseInt(st.nextToken());
-					if (board[i][j] >= 6 && board[i][j] <= 10) { // 웜홀
-						wormhole[board[i][j]].add(new Pair(i, j));
+				for (int j=1; j<=N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] >= 6) {	// 웜홀
+						int n = map[i][j];
+						if (worm[n][0] == 0) {	// 첫번째 웜홀인 경우 
+							worm[n][0] = i;
+							worm[n][1] = j;
+						} else {	// 두번째 웜홀인 경우 
+							worm[n][2] = i;
+							worm[n][3] = j;
+						}
 					}
 				}
 			}
-
-			maxScore = -1;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (board[i][j] != 0)
-						continue;
-					for (int dir = 0; dir < 4; dir++) {
-						play(i, j, dir);
+			// 가장자리 칸 5번 블록으로 채우기 
+			for (int i=0; i < N+2; i++) {
+				map[i][0] = 5;
+				map[i][N+1] = 5;
+			}
+			for (int j=0; j < N+2; j++) {
+				map[0][j] = 5;
+				map[N+1][j] = 5;
+			}
+			
+			res = Integer.MIN_VALUE;
+			
+			// 모든 지점에서 탐색 시작 
+			for (int i=1; i<= N; i++) {
+				for (int j=1; j<=N; j++) {	// (i, j): 출발 위치 
+					if (map[i][j] != 0)	continue;	// 빈칸에서만 시작 가능 
+					for (int dir = 0; dir < 4; dir++) {	// 4방탐색 
+						int nx = i;
+						int ny = j;
+						int nd = dir;
+						int score = 0;
+						while(true) {
+							nx += dx[nd];
+							ny += dy[nd];
+							int n = map[nx][ny];
+							
+							// 원위치 or 블랙홀
+							if ((nx == i && ny == j) || n == -1)	break;
+							
+							if (n >= 6) {	// 웜홀
+								if (nx == worm[n][0] && ny == worm[n][1]) {	// 첫번째 웜홀
+									nx = worm[n][2];
+									ny = worm[n][3];
+								} else {	// 두번째 웜홀
+									nx = worm[n][0];
+									ny = worm[n][1];
+								}
+							} else {	// 블록 or 빈칸
+								nd = dd[n][nd];	// 방향 전환
+								if (n != 0) score++;
+							}
+						}
+						res = Math.max(res, score);
 					}
 				}
 			}
-
-			sb.append(maxScore).append("\n");
+			
+			
+			sb.append("#").append(tc).append(" ").append(res).append("\n");
 		}
+		
 		System.out.println(sb);
-	}
-
-	private static void play(int initX, int initY, int dir) {
-		int score = 0;
-		int x = initX;
-		int y = initY;
-
-		while (true) {
-			// 블록일 경우 방향 전환 
-			if (board[x][y] >= 1 && board[x][y] <= 5) {
-				score++;
-				switch (board[x][y]) {
-				case 1:
-					if (dir == 3) {
-						dir -= 3;
-					} else if (dir == 2) {
-						dir--;
-					} else {
-						dir = (dir + 2) % 4;
-					}
-					break;
-				case 2:
-					if (dir == 3) {
-						dir--;
-					} else if (dir == 0) {
-						dir++;
-					} else {
-						dir = (dir + 2) % 4;
-					}
-					break;
-				case 3:
-					if (dir == 1) {
-						dir++;
-					} else if (dir == 0) {
-						dir += 3;
-					} else {
-						dir = (dir + 2) % 4;
-					}
-					break;
-				case 4:
-					if (dir == 1) {
-						dir--;
-					} else if (dir == 2) {
-						dir++;
-					} else {
-						dir = (dir + 2) % 4;
-					}
-					break;
-				case 5:
-					dir = (dir + 2) % 4;
-					break;
-				}
-			}
-
-			// 블록인 경우는 방향바뀌고, 아니면 원래 방향으로 감
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-
-			// 벽일 경우
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
-				score++;
-				dir = (dir + 2) % 4;
-				nx = x;
-				ny = y;
-			}
-
-			// 처음 위치로 돌아왔을 경우 or 블랙홀일 경우 종료
-			if ((nx == initX && ny == initY) || board[nx][ny] == -1) {
-				break;
-			}
-
-			// 웜홀일 경우
-			if (board[nx][ny] >= 6 && board[nx][ny] <= 10) {
-				for (Pair tmp : wormhole[board[nx][ny]]) {
-					if (tmp.x == nx && tmp.y == ny) continue;
-					nx = tmp.x;
-					ny = tmp.y;
-					break;
-				}
-			}
-			// 다음칸 이동 
-			x = nx;
-			y = ny;
-		}
-		maxScore = Math.max(maxScore, score);
 	}
 }
