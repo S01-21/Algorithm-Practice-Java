f6abdc3c568325b9284a9244f136ccb4c90a5c32
JS Kwon <89765285+S01-21@users.noreply.github.com>
2024-05-26 01:40:00 +0900

    [Gold III] Title: 게임 개발, Time: 196 ms, Memory: 19528 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+// 골드 3. 게임 개발 
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, sum;
+	static PriorityQueue<Node> pq;
+	static Node[] adjList;
+	static class Node implements Comparable<Node>{
+		int num, time;
+		Node next;
+		public Node(int num, int time, Node next) {
+			super();
+			this.num = num;
+			this.time = time;
+			this.next = next;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return this.time - o.time;
+		}
+		@Override
+		public String toString() {
+			return "Node [num=" + num + ", time=" + time + ", next=" + next + "]";
+		}
+		
+	}
+	static int[] inDegree, times;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		inDegree = new int[N+1];
+		adjList = new Node[N+1];
+		times = new int[N+1];
+		StringTokenizer st = null;
+		for (int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int time = Integer.parseInt(st.nextToken());	// 건물 짓는 데 걸리는 시간 
+			times[i] = time;
+			while (true) {
+				int num = Integer.parseInt(st.nextToken());
+				if (num == -1)	break;
+				adjList[num] = new Node(i, time, adjList[num]);
+				inDegree[i]++;
+			}
+			
+		}
+		pq = new PriorityQueue<>();
+		for (int j=1; j<=N; j++) {
+			if (inDegree[j] == 0) {
+				pq.offer(new Node(j, times[j], null));
+			}
+		}
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			for (Node tmp = adjList[cur.num]; tmp != null; tmp = tmp.next) {
+				inDegree[tmp.num]--;
+				if (inDegree[tmp.num] == 0) {
+					times[tmp.num] += times[cur.num];
+					pq.offer(new Node(tmp.num, times[tmp.num], null));
+				}
+			}
+		}
+		
+		for (int i=1; i<=N; i++) {
+			sb.append(times[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+}
