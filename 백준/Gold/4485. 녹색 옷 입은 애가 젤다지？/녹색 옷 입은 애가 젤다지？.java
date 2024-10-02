863ba0ab52e191bf0cdbd3ac244e77bc0ce4c9a4
JS Kwon <lop080@ewhain.net>
2024-10-03 00:22:01 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 160 ms, Memory: 18716 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -3,38 +3,38 @@ import java.io.*;
 
 public class Main {
 	static StringBuilder sb = new StringBuilder();
-	static int N;
-	static int res;
+	static int N, res;
 	static int[][] map;
-	static boolean[][] vis;
 	static int[][] minDist;
+	static boolean[][] vis;
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,-1,1};
+	
 	static class Pair implements Comparable<Pair>{
 		int x, y, weight;
-
 		public Pair(int x, int y, int weight) {
-			super();
 			this.x = x;
 			this.y = y;
 			this.weight = weight;
 		}
-
-		@Override
 		public int compareTo(Pair o) {
 			return this.weight - o.weight;
 		}
-		
 	}
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+//		M = Integer.parseInt(st.nextToken());
 		int tc = 1;
 		while (true) {
 			N = Integer.parseInt(br.readLine());
-			if (N == 0)	break;
+			if (N == 0) break;
+			sb.append("Problem ").append(tc).append(": ");
+			
 			map = new int[N][N];
+			vis = new boolean[N][N];
 			minDist = new int[N][N];
-			StringTokenizer st = null;
 			for (int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j=0; j<N; j++) {
@@ -43,38 +43,40 @@ public class Main {
 				}
 			}
 			
-			dijkstra();
-			
-			res = minDist[N-1][N-1];
-			sb.append("Problem ").append(tc).append(": ").append(res).append("\n");
+			res = dijkstra();
+			sb.append(res).append("\n");
 			tc++;
 		}
+		
 		System.out.println(sb);
 		br.close();
 	}
-	private static void dijkstra() {
-		minDist[0][0] = map[0][0];
+	private static int dijkstra() {
 		PriorityQueue<Pair> pq = new PriorityQueue<>();
-		pq.offer(new Pair(0,0,map[0][0]));
+		minDist[0][0] = map[0][0];
+		pq.offer(new Pair(0, 0, minDist[0][0]));
 		
 		while (!pq.isEmpty()) {
-			Pair stopover = pq.poll();
+			Pair cur = pq.poll();
+			if (vis[cur.x][cur.y])	continue;
 			
-			if (minDist[stopover.x][stopover.y]!= stopover.weight)	continue;
-			
-			if (stopover.x == N-1 && stopover.y == N-1) {
-				break;
-			}
+			vis[cur.x][cur.y] = true;
 			
 			for (int dir = 0; dir < 4; dir++) {
-				int nx = stopover.x + dx[dir];
-				int ny = stopover.y + dy[dir];
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N)	continue;
-				if (minDist[nx][ny] > minDist[stopover.x][stopover.y] + map[nx][ny]) {
-					minDist[nx][ny] = minDist[stopover.x][stopover.y] + map[nx][ny];
+				int nx = cur.x + dx[dir];
+				int ny = cur.y + dy[dir];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+				if (vis[nx][ny])	continue;
+				if (minDist[nx][ny] > minDist[cur.x][cur.y]+ map[nx][ny]) {
+					minDist[nx][ny] = minDist[cur.x][cur.y]+ map[nx][ny];
 					pq.offer(new Pair(nx, ny, minDist[nx][ny]));
 				}
 			}
+			
 		}
+		
+		return minDist[N-1][N-1];
 	}
+	
+	
 }
