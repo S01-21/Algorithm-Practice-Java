a87e433d47a758a50d192a9a822b6d48af32cce8
JS Kwon <lop080@ewhain.net>
2024-10-05 23:55:21 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 408 ms, Memory: 63080 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,88 +1,96 @@
 import java.util.*;
 import java.io.*;
 
-// 특정한 최단 경로 
 public class Main {
 	static StringBuilder sb = new StringBuilder();
-	static int N, E;
+	static int N, E, v1, v2, res;
+	static boolean[] vis;
 	static int[] minDist;
 	static Node[] adjList;
-	static boolean[] vis;
-	static final int INF = Integer.MAX_VALUE;
-	static class Node{
+	static class Node implements Comparable<Node>{
 		int num, weight;
 		Node next;
-		public Node(int num, int weight, Node next) {
-			super();
+		public Node(int num, Node next, int weight) {
 			this.num = num;
-			this.weight = weight;
 			this.next = next;
+			this.weight = weight;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return this.weight - o.weight;
 		}
 	}
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());	// 정점 개수
-		E = Integer.parseInt(st.nextToken());	// 간선 개수
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
 		
-		adjList = new Node[N + 1];	// 0번인덱스 사용 x
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		
-		for (int i=0; i<E; i++) {
+		adjList = new Node[N+1];
+		minDist = new int[N+1];
+		vis = new boolean[N+1];
+		
+		for (int i = 0; i< E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
-			adjList[from] = new Node(to, weight, adjList[from]);
-			adjList[to] = new Node(from, weight, adjList[to]);
+			
+			adjList[from] = new Node(to, adjList[from], weight);
+			adjList[to] = new Node(from, adjList[to], weight);
 		}
 		st = new StringTokenizer(br.readLine());
-		int	stop1 = Integer.parseInt(st.nextToken());
-		int stop2 = Integer.parseInt(st.nextToken());
+		v1 = Integer.parseInt(st.nextToken());
+		v2 = Integer.parseInt(st.nextToken());
 		
-		int res = INF;
-		int tmp1 = dijkstra(1, stop1);
-		int tmp2 = dijkstra(stop1, stop2);
-		int tmp3 = dijkstra(stop2, N);
+		res = Integer.MAX_VALUE;
+		
+		int tmp1 = dijkstra(1, v1);
+		int tmp2 = dijkstra(v1, v2);
+		int tmp3 = dijkstra(v2, N);
+		
 		if (tmp1 != -1 && tmp2 != -1 && tmp3 != -1) {
 			res = tmp1 + tmp2 + tmp3;
 		}
-		
-		tmp1 = dijkstra(1, stop2);
-		tmp3 = dijkstra(stop1, N);
+		tmp1 = dijkstra(1, v2);
+		tmp3 = dijkstra(v1, N);
 		if (tmp1 != -1 && tmp2 != -1 && tmp3 != -1) {
 			res = Math.min(res, tmp1 + tmp2 + tmp3);
 		}
+		if (res == Integer.MAX_VALUE) {
+			res = -1;
+		}
 		
-		System.out.println(res != INF ? res : -1);
+		System.out.println(res);
+		br.close();
 	}
-	
-	private static int dijkstra(int start, int end) {
-		minDist = new int[N + 1];
-		vis = new boolean[N + 1];
-		Arrays.fill(minDist, INF);
-		minDist[start] = 0;
+	static int dijkstra(int start, int end) {
 		
-		for (int i = 0; i < N; i++) {
-			int min = INF;
-			int stopover = -1;
-			for (int j=1; j<=N; j++) {
-				if (vis[j])	continue;
-				if (min > minDist[j]) {
-					min = minDist[j];
-					stopover = j;
-				}
-			}
-			if (stopover == -1)	break;
-			vis[stopover] = true;
-			if (stopover == end) break;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		Arrays.fill(vis, false);
+		Arrays.fill(minDist, Integer.MAX_VALUE);
+		minDist[start] = 0;
+		pq.offer(new Node(start, null, minDist[start]));
+		
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
 			
-			for (Node tmp = adjList[stopover]; tmp != null; tmp = tmp.next) {
-				if (minDist[tmp.num] > min + tmp.weight) {
-					minDist[tmp.num] = min + tmp.weight;
+			if (vis[cur.num])	continue;
+			
+			vis[cur.num] = true;
+			
+			if (cur.num == end)	break;
+			
+			for (Node tmp = adjList[cur.num]; tmp != null; tmp = tmp.next) {
+				if (vis[tmp.num])	continue;
+				if (minDist[tmp.num] > minDist[cur.num] + tmp.weight) {
+					minDist[tmp.num] = minDist[cur.num] + tmp.weight;
+					pq.offer(new Node(tmp.num, null, minDist[tmp.num]));
 				}
 			}
 		}
-		return minDist[end] != INF ? minDist[end] : -1;
+		return minDist[end] != Integer.MAX_VALUE ? minDist[end] : -1;
 	}
-	
 }
