bb3e931c3ede8bbe08901b77358c0b1f547d5762
JS Kwon <lop080@ewhain.net>
2025-01-09 00:46:51 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int[] dp;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		dp = new int[N+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		for (int i = 2; i <= N; i++) {
+			dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+		}
+		
+		System.out.println(dp[N]);
+		br.close();
+	}
+}
