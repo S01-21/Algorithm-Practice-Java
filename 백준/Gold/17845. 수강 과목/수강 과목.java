7582e758ca06c452ccb1b5b7ce5df3809eb1bdfc
Jeongsol Kwon <lop080@ewhain.net>
2025-05-17 13:31:20 +0900

    [Gold V] Title: 수강 과목, Time: 152 ms, Memory: 49544 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -1,42 +1,46 @@
 import java.util.*;
 import java.io.*;
 
-// 골드 5. 수강 과목 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
 	static int N, K;
-	static int[][] info;
+	static Subject[] list;
 	static int[][] dp;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static class Subject{
+		int imp, time;
+		public Subject(int imp, int time) {
+			this.imp = imp;
+			this.time = time;
+		}
+	}
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new  InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());	// 최대 공부시간 
-		K = Integer.parseInt(st.nextToken());	// 과목 수 
+		N = Integer.parseInt(st.nextToken());	// 최대 공부시간
+		K = Integer.parseInt(st.nextToken());	// 과목 수
+		list = new Subject[K+1];
+		dp = new int[K+1][N+1];
 		
-		info = new int[K+1][2];	// [0]: 중요도, [1]: 필요한 공부시간 
-		for (int i=1; i<=K; i++) {
+		for (int i = 1; i <= K; i++) {
 			st = new StringTokenizer(br.readLine());
-			info[i][0] = Integer.parseInt(st.nextToken());	// 중요도 
-			info[i][1] = Integer.parseInt(st.nextToken());	// 필요한 공부시간 
+			int imp = Integer.parseInt(st.nextToken());
+			int time = Integer.parseInt(st.nextToken());
+			list[i] = new Subject(imp, time);
 		}
 		
-		dp = new int[K+1][N+1];
-		for (int i=1; i<=K; i++) {	// 모든 과목 
-			for (int j=1; j<=N; j++) {
-				int I = info[i][0];	// 중요도 
-				int T = info[i][1];	// 필요 공부시간 
+		for (int i = 1; i <= K; i++) {
+			for (int j = 0; j <= N; j++) {
+				int imp = list[i].imp;
+				int time = list[i].time;
 				
-				
-				if (T > j) {
+				if (time <= j) {
+					dp[i][j] = Math.max(dp[i-1][j-time] + imp, dp[i-1][j]);
+				} else {
 					dp[i][j] = dp[i-1][j];
-				}
-				else {
-					int include = I + dp[i-1][j-T];
-					int exclude = dp[i-1][j];
-					dp[i][j] = Math.max(include, exclude);
 				}
 			}
 		}
+		
 		System.out.println(dp[K][N]);
 		br.close();
 	}
